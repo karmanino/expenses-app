b393c9250da1dd1db69a9fa2302cb2102eda7c26
Kuki <karmanino@gmail.com>
2022-08-09 02:40:46 -0300

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+// Modules
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { StatsComponent } from './expenses/stats/stats.component';
+import { DetailComponent } from './expenses/detail/detail.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ExpensesComponent,
+    StatsComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { DetailComponent } from '../expenses/detail/detail.component';
+import { ExpensesComponent } from '../expenses/expenses.component';
+import { StatsComponent } from '../expenses/stats/stats.component';
+
+export const dashboardRoutes: Routes = [
+  { path: '', component: StatsComponent },
+  { path: 'movement', component: ExpensesComponent },
+  { path: 'detail', component: DetailComponent },
+];
